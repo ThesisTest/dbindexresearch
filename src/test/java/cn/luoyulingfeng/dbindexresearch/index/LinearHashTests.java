6ff377314d729797306854851ea55a8cf7b3502f
泠风 <46060451+1014661165@users.noreply.github.com>
2019-12-09 18:10:29 +0800

    上传文档

--- src/test/java/cn/luoyulingfeng/dbindexresearch/index/LinearHashTests.java
+++ src/test/java/cn/luoyulingfeng/dbindexresearch/index/LinearHashTests.java
@@ -7,10 +7,15 @@ import cn.luoyulingfeng.dbindexresearch.model.Book;
 import cn.luoyulingfeng.dbindexresearch.model.BookBorrowRecord;
 import cn.luoyulingfeng.dbindexresearch.model.Student;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.assertj.core.util.DateUtil;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -74,12 +79,38 @@ public class LinearHashTests {
 
     @Test
     public void testReadAllLines()throws IOException{
-        BufferedReader reader = new BufferedReader(new FileReader("csvdata/BooksName.txt"));
-        String line = null;
-        List<String> names = new ArrayList<>(10000000);
-        while ((line = reader.readLine()) != null){
-            names.add(line);
+        long start = System.currentTimeMillis();
+        FileReader reader = new FileReader("csvdata/Books.csv");
+        CSVParser csvParser = CSVFormat.DEFAULT.parse(reader);
+
+        List<Book> books = new ArrayList<>();
+        int cnt = 0;
+        for (CSVRecord record: csvParser){
+            if (cnt > 1000000){
+                break;
+            }
+            Book book = new Book();
+            book.setId(Integer.parseInt(record.get(0)));
+            book.setBookIsbn(record.get(1));
+            book.setBookBookcaseNo(record.get(2));
+            book.setBookNameCn(record.get(3));
+            book.setBookNameEn(record.get(4));
+            book.setBookAuthor(record.get(5));
+            book.setBookPress(record.get(6));
+            book.setBookVersion(Integer.parseInt(record.get(7)));
+            book.setBookPrice(Float.parseFloat(record.get(8)));
+            book.setBookSeries(record.get(9));
+            book.setBookCover(record.get(10));
+            book.setBookTotalPage(Integer.parseInt(record.get(11)));
+            book.setBookStatus(Integer.parseInt(record.get(12)));
+            books.add(book);
+            cnt++;
         }
-        reader.close();
+        long end = System.currentTimeMillis();
+        System.out.printf("time cost %f s\n", (end - start)/1e3);
+        csvParser.close();
+        for (int i=0; i<5; i++){
+            System.out.println(books.get(i));
+        }
     }
 }
